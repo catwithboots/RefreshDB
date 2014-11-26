0b5560c6ec25419625289080e468d1d39b8d31bd
cihatgenc <cihat@gencnet.nl>
2014-11-26 22:34:24 +0100

    Added links to the solutions item

--- RefreshDB.WebAPI.Backend/Properties/AssemblyInfo.cs
+++ RefreshDB.WebAPI.Backend/Properties/AssemblyInfo.cs
@@ -22,14 +22,3 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("c900910a-3180-4c23-9fc8-657be87ef2c1")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Revision and Build Numbers 
-// by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
