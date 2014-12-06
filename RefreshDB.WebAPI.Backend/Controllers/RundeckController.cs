66f916aae030aaf22a662c70c9c8b61ee67323e5
cihatgenc <cihat@gencnet.nl>
2014-12-06 16:51:48 +0100

    Added a rundeck controller

---
+++ RefreshDB.WebAPI.Backend/Controllers/RundeckController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+using RefreshDB.Database.Framework;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace RefreshDB.WebAPI.Backend.Controllers
+{
+    public class RundeckController : ApiController
+    {
+        public dynamic GetAllEnvironments()
+        {
+            dynamic env = new EnvironmentsController();
+            {
+
+                List<RefreshDB.Database.Framework.Environment> list = env.Get();
+
+                Func<RefreshDB.Database.Framework.Environment, JObject> objToJson =
+                    o => new JObject(
+                            new JProperty("name", o.name),
+                            new JProperty("value", o.id));
+
+                string result = new JObject(new JArray(list.Select(objToJson))).ToString();
+
+                return Json(result);
+            }
+        }
+    }
+}
