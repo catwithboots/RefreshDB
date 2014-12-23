5452b5206318f943087afa56f5b650dc58b45e80
Cihat Genc <cihat@gencnet.nl>
2014-12-23 10:55:17 +0100

    Removed all PsRemote code

--- RefreshDB.WebAPI.Backend/Controllers/DatabasesController.cs
+++ RefreshDB.WebAPI.Backend/Controllers/DatabasesController.cs
@@ -18,57 +18,6 @@ namespace RefreshDB.WebAPI.Backend.Controllers
 {
     public class DatabasesController : ApiController
     {
-        public List<string> GetDatabasesByInstance(int id)
-        {
-            var instcnt = new InstancesController();
-
-            Instance instobj = instcnt.GetInstanceById(id);
-
-            string WinrmPort = System.Configuration.ConfigurationManager.AppSettings["WinrmPort"];
-
-            string strPrefix;
-            if (System.Configuration.ConfigurationManager.AppSettings["WinrmSSL"] == "true")
-            {
-                strPrefix = "https://";
-            }
-            else
-            {
-                strPrefix = "http://";
-            }
-
-            string winrmurl = strPrefix + instobj.servername + ":" + WinrmPort + "/wsman";
-            string schema = @"http://schemas.microsoft.com/powershell/Microsoft.PowerShell";
-            string username = System.Configuration.ConfigurationManager.AppSettings[@"WinrmUsername"];
-            string password = System.Configuration.ConfigurationManager.AppSettings[@"WinrmPassword"];
-            
-            List<string> psresults = new List<string>();
-
-            // Execute powershell script to return database names via winrm
-            Runspace remoteRunspace = null;
-
-            WSManHelper.openRunspace(winrmurl, schema, username, password, ref remoteRunspace);
-
-            using (PowerShell powershell = PowerShell.Create())
-            {
-                powershell.Runspace = remoteRunspace;
-                string psscript = File.ReadAllText(System.Web.Hosting.HostingEnvironment.MapPath("~/PowershellScripts/RunSQLStatement.ps1"));
-                powershell.AddScript(psscript);
-                powershell.Commands.AddParameter("SqlServer", instobj.name.ToString());
-                powershell.Commands.AddParameter("SqlCatalog", "master");
-                powershell.Commands.AddParameter("SqlQuery", "select name from sys.databases where database_id > 4 and name not in ('SBPTools')");
-                powershell.Invoke();
-                var results = powershell.Invoke();
-                remoteRunspace.Close();
-                foreach (PSObject obj in results)
-                {
-                    psresults.Add(obj.Properties["name"].Value.ToString());
-                }
-            }
-
-            return psresults;
-
-        }
-        
         public List<string> GetDbsByInstance(int id)
         {
             //throw new HttpResponseException(new HttpResponseMessage(HttpStatusCode.NotImplemented));
