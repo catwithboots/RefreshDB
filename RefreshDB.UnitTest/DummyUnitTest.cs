d56533cb386d53b2cab2cb4d5c8a48c421f4746e
Cihat Genc <cihat@gencnet.nl>
2015-01-07 15:09:53 +0100

    Let's try to xUnit test via Jenkins

--- RefreshDB.UnitTest/DummyUnitTest.cs
+++ RefreshDB.UnitTest/DummyUnitTest.cs
@@ -15,11 +15,11 @@ namespace RefreshDB.UnitTest
             Assert.Equal(4, Add(2, 2));
         }
 
-        [Fact]
-        public void FailingTest()
-        {
-            Assert.Equal(5, Add(2, 2));
-        }
+        //[Fact]
+        //public void FailingTest()
+        //{
+        //    Assert.Equal(5, Add(2, 2));
+        //}
 
         int Add(int x, int y)
         {
